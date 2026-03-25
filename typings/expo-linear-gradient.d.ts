623f7f58e0013c52650efc66682d5f6817dd7aa2
Dhanush <dk255767@gmail.com>
2026-03-25 12:11:41 +0530

    Added new finance screen

--- typings/expo-linear-gradient.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'expo-linear-gradient' {
-  import { ComponentType } from 'react';
-  import { ViewProps } from 'react-native';
-
-  export const LinearGradient: ComponentType<ViewProps & { colors?: string[] }>;
-  export default LinearGradient;
-}
